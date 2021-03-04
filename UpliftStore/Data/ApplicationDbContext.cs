0723364ba4ae4751187442f3e76fe02f99ae711f
Nelson Roza Junior <jr.1403@hotmail.com>
2021-03-04 00:14:13 -0300

    Add _CookieConsentPartial shared view

--- UpliftStore/Data/ApplicationDbContext.cs
+++ UpliftStore/Data/ApplicationDbContext.cs
@@ -1,15 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace UpliftStore.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
     }

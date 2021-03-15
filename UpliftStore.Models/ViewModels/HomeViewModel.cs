4482662aac42f0cbcb005e83f71444c947bbf304
Nelson Roza Junior <jr.1403@hotmail.com>
2021-03-14 23:04:39 -0300

    Home view implementation

---
+++ UpliftStore.Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace UpliftStore.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Category> Categories { get; set; }
+
+        public IEnumerable<Service> Services { get; set; }
+    }
+}
--- UpliftStore/Areas/Customer/Controllers/HomeController.cs
+++ UpliftStore/Areas/Customer/Controllers/HomeController.cs
@@ -1,23 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Linq;
+using UpliftStore.DataAccess.Data.Repository.Interfaces;
 using UpliftStore.Models;
+using UpliftStore.Models.ViewModels;
 
 namespace UpliftStore.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUnitOfWork unitOfWork, ILogger<HomeController> logger)
         {
+            _unitOfWork = unitOfWork;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                Services = _unitOfWork.ServiceRepository.GetAll(includeProperties: "Frequency"),
+                Categories = _unitOfWork.CategoryRepository.GetAll(orderBy: q => q.OrderBy(c => c.DisplayOrder))
+            };
+
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
@@ -28,7 +39,10 @@ namespace UpliftStore.Areas.Customer.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel 
+            { 
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier 
+            });
         }
     }
 }
